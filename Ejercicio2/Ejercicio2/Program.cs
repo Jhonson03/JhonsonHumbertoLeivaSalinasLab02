2641cb389746f11ee5ea66db91c1ad7dafed09a3
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-10-07 16:10:50 -0600

    Ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Ejercicio2
+
+DateTime FechaActual = DateTime.Now;
+
+DateTime FechaNavidad = new DateTime(FechaActual.Year, 12, 25);
+DateTime FechaAñoNuevo = new DateTime(FechaActual.Year + 1, 1, 1);
+int DiasNavidad = (FechaNavidad - FechaActual).Days;
+int DiasAñoNuevo = (FechaAñoNuevo - FechaActual).Days;
+
+Console.WriteLine($"Día actual: {FechaActual.Day}");
+Console.WriteLine($"Mes actual: {FechaActual.Month}");
+Console.WriteLine($"Año actual: {FechaActual.Year}");
+Console.WriteLine($"Días faltantes para Navidad: {DiasNavidad}");
+Console.WriteLine($"Días faltantes para Año Nuevo: {DiasAñoNuevo}");
