d33a5da52f5dcc61066784e28811906862f1abd9
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-10-07 15:34:49 -0600

    Ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Ejercicio 1
+
+Console.WriteLine(@"Bienvenido a su calculadora de distancia entre dos puntos
+
+A continuacion necesito que ingrese P(x, y) y Q(h,k)");
+
+Console.Write("Ingrese el valor de x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el valor de y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el valor de h: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el valor de k: ");
+double k = Convert.ToDouble(Console.ReadLine());
+
+double r = Math.Sqrt(Math.Pow(x - h, 2) + Math.Pow(y - k,2));
+
+Console.WriteLine($"La distancia de los puntos P({x}, {y}) y Q({h}, {k}) es: {r}");
